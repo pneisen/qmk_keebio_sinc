9188dbda67e8d9590044866c6b0042cb09d367c0
Pete Neisen <pete@neisen.xyz>
2023-09-08 01:16:27 -0600

    Added a toggle for one shot since it might not work well with gaming

--- keymap.c
+++ keymap.c
@@ -12,13 +12,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,       KC_EQL,  XXXXXXX,  KC_BSPC, KC_INS,
     KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS,  KC_DEL,
     KC_LCTL,       KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,  KC_HOME,
-    OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT), KC_UP,   KC_END,
+    OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT), KC_UP,   KC_END,
     KC_LCTL,       KC_LGUI, KC_LALT, MO(1),   XXXXXXX,  KC_SPC,           XXXXXXX, KC_SPC,  KC_RALT, KC_RCTL, XXXXXXX,       KC_LEFT, KC_DOWN,  KC_RGHT
   ),
   [_OTHER] = LAYOUT_80(
     _______,       _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,        _______, _______,  _______,
     _______,       _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,        _______, XXXXXXX,  KC_DEL,  KC_MNXT,
-    _______,       _______, _______, _______, QK_RBT,   _______, _______, _______, _______, KC_PWR,  _______, _______,        _______, _______,  KC_MPRV,
+    _______,       _______, _______, _______, QK_RBT,   OS_TOGG, _______, _______, _______, KC_PWR,  _______, _______,        _______, _______,  KC_MPRV,
     KC_CAPS,       _______, KC_SLEP, _______, _______,  _______, _______, _______, _______, QK_LOCK, _______, _______,        _______, _______,
     _______,       _______, _______, KC_CALC,  _______, QK_BOOT, _______, _______, _______, _______, _______, _______,        _______, _______,
     _______,       _______, _______, _______, XXXXXXX,  _______,          XXXXXXX, _______, _______, _______, XXXXXXX,        _______, _______,  _______
